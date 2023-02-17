771a2ba0af600d9ec8b4a99cdc265b7f6771e6c8
MendozaJr69 <fmaciendozah99@gmail.com>
2023-02-17 10:58:58 -0600

    First commit

---
+++ usuario.php
@@ -0,0 +1,224 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+
+<title>Carrito de compras</title>
+</head>
+<body>
+
+
+<?php 
+
+$carrito_mio=$_SESSION['carrito'];
+$_SESSION['carrito']=$carrito_mio;
+
+// contamos nuestro carrito
+if(isset($_SESSION['carrito'])){
+    for($i=0;$i<=count($carrito_mio)-1;$i ++){
+    if($carrito_mio[$i]!=NULL){ 
+    $total_cantidad = $carrito_mio['cantidad'];
+    $total_cantidad ++ ;
+    $totalcantidad += $total_cantidad;
+    }}}
+?>
+
+<!-- NAVBAR -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container-fluid">
+    <a class="navbar-brand" href="#">Mi tienda</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-bs-toggle="modal" data-bs-target="#modal_cart" style="color: red;"><i class="fas fa-shopping-cart"></i> <?php echo $totalcantidad; ?></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- END NAVBAR -->
+
+
+
+<!-- MODAL CARRITO -->
+<div class="modal fade" id="modal_cart" tabindex="-1"  aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">ventana del total</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+   
+   
+     
+			<div class="modal-body">
+				<div>
+					<div class="p-2">
+						<ul class="list-group mb-3">
+							<?php
+							if(isset($_SESSION['carrito'])){
+							$total=0;
+							for($i=0;$i<=count($carrito_mio)-1;$i ++){
+							if($carrito_mio[$i]!=NULL){
+						
+            ?>
+							<li class="list-group-item d-flex justify-content-between lh-condensed">
+								<div class="row col-12" >
+									<div class="col-6 p-0" style="text-align: left; color: #000000;"><h6 class="my-0">Cantidad: <?php echo $carrito_mio[$i]['cantidad'] ?> : <?php echo $carrito_mio[$i]['titulo']; // echo substr($carrito_mio[$i]['titulo'],0,10); echo utf8_decode($titulomostrado)."..."; ?></h6>
+									</div>
+									<div class="col-6 p-0"  style="text-align: right; color: #000000;" >
+									<span   style="text-align: right; color: #000000;"><?php echo $carrito_mio[$i]['precio'] * $carrito_mio[$i]['cantidad'];    ?> pesitos</span>
+									</div>
+								</div>
+							</li>
+							<?php
+							$total=$total + ($carrito_mio[$i]['precio'] * $carrito_mio[$i]['cantidad']);
+							}
+							}
+							}
+							?>
+							<li class="list-group-item d-flex justify-content-between">
+							<span  style="text-align: left; color: #000000;">Total $</span>
+							<strong  style="text-align: left; color: #000000;"><?php
+							if(isset($_SESSION['carrito'])){
+							$total=0;
+							for($i=0;$i<=count($carrito_mio)-1;$i ++){
+							if($carrito_mio[$i]!=NULL){ 
+							$total=$total + ($carrito_mio[$i]['precio'] * $carrito_mio[$i]['cantidad']);
+							}}}
+							echo $total; ?> pesos</strong>
+							</li>
+                                                        
+						</ul>
+					</div>
+				</div>
+			</div>
+			
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <a type="button" class="btn btn-primary" href="borrarcarro.php">Vaciar carrito</a>
+        <!-- <a type="button" class="btn btn-primary" name="">Comprar carrito</a> -->
+        <input type="submit" class="btn btn-primary" name="btncomprar" value="Comprar carrito">
+
+      </div>
+      <?php
+        if (isset($_POST["btncomprar"])){
+                $compras=array($total);
+                echo $compras[0];
+        }
+        ?>
+    </div>
+  </div>
+</div>
+<!-- END MODAL CARRITO -->
+
+
+
+
+
+<!-- ARTICULOS -->
+<div class="container mt-5">
+<div class="row" style="justify-content: center;">
+
+<div class="card m-4" style="width: 18rem;">
+        <form id="formulario" name="formulario" method="post" action="cart.php">
+        <input name="precio" type="hidden" id="precio" value="20.00" />
+        <input name="titulo" type="hidden" id="titulo" value="coca" />
+        <input name="cantidad" type="hidden" id="cantidad" value="1" class="pl-2" />
+        <img src="img/coca.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                        <h5 class="card-title">coca cola</h5>
+                        <p class="card-text">para que te refresques - Precio $20.00</p>
+                        <button class="btn btn-primary" type="submit" ><i class="fas fa-shopping-cart"></i> Añadir al carrito</button>
+                </div>
+        </form>
+</div>
+
+
+
+<div class="card m-4" style="width: 18rem;">
+        <form id="formulario" name="formulario" method="post" action="cart.php">
+        <input name="precio" type="hidden" id="precio" value="15.00" />
+        <input name="titulo" type="hidden" id="titulo" value="dona" />
+        <input name="cantidad" type="hidden" id="cantidad" value="1" class="pl-2" />
+        <img src="img/dona.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                        <h5 class="card-title">Dona Gansito</h5>
+                        <p class="card-text">Para recordarme - Precio $15.00</p>
+                        <button class="btn btn-primary" type="submit" ><i class="fas fa-shopping-cart"></i> Añadir al carrito</button>
+                </div>
+        </form>
+</div>
+
+
+<div class="card m-4" style="width: 18rem;">
+        <form id="formulario" name="formulario" method="post" action="cart.php">
+        <input name="precio" type="hidden" id="precio" value="18.00" />
+        <input name="titulo" type="hidden" id="titulo" value="Zote" />
+        <input name="cantidad" type="hidden" id="cantidad" value="1" class="pl-2" />
+        <img src="img/zote.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                        <h5 class="card-title">Jabon Zote</h5>
+                        <p class="card-text">Para quitar las manchas profundas - Precio $18.00</p>
+                        <button class="btn btn-primary" type="submit" ><i class="fas fa-shopping-cart"></i> Añadir al carrito</button>
+                </div>
+        </form>
+</div>
+
+
+<div class="card m-4" style="width: 18rem;">
+        <form id="formulario" name="formulario" method="post" action="cart.php">
+        <input name="precio" type="hidden" id="precio" value="80.00" />
+        <input name="titulo" type="hidden" id="titulo" value="Olla" />
+        <input name="cantidad" type="hidden" id="cantidad" value="1" class="pl-2" />
+        <img src="img/olla.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                        <h5 class="card-title">Olla de barro</h5>
+                        <p class="card-text">para tener un sabor diferente - Precio $80.00</p>
+                        <button class="btn btn-primary" type="submit" ><i class="fas fa-shopping-cart"></i> Añadir al carrito</button>
+                </div>
+        </form>
+</div>
+
+
+<div class="card m-4" style="width: 18rem;">
+        <form id="formulario" name="formulario" method="post" action="cart.php">
+        <input name="precio" type="hidden" id="precio" value="45.00" />
+        <input name="titulo" type="hidden" id="titulo" value="Sombrero" />
+        <input name="cantidad" type="hidden" id="cantidad" value="1" class="pl-2" />
+        <img src="img/sombrero.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                        <h5 class="card-title">Sombrero mexicano</h5>
+                        <p class="card-text">Para parecerte mas mexicano de lo que ya eres - Precio $45.00</p>
+                        <button class="btn btn-primary" type="submit" ><i class="fas fa-shopping-cart"></i> Añadir al carrito</button>
+                </div>
+        </form>
+</div>
+
+
+</div>
+</div>
+<!-- END ARTICULOS -->
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/js/bootstrap.min.js"></script>
+</body>
+</html>
